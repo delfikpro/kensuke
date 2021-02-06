a938387fe4d3726dff799f8a83f34281444a27ab
Andrew Anfanik <me@anfanik.me>
2021-02-06 03:35:44 +0300

    StatStorageImpl

--- src/storage.ts
+++ src/storage.ts
@@ -1,3 +1,4 @@
+import * as mongoose from 'mongoose';
 
 export type Stats = Record<string, any>
 
@@ -6,5 +7,41 @@ export interface StatStorage {
     provideStats(uuid: string): Promise<Stats>;
 
     saveStats(uuid: string, stats: Stats): Promise<void>;
+
+}
+
+export class StatStorageImpl implements StatStorage {
+
+    model = mongoose.model('Stats', new mongoose.Schema({
+        uuid: String,
+        data: Object
+    }));
+
+    constructor(url: string) {
+        mongoose.connect(url);
+    }
+
+    provideStats(uuid: string): Promise<Stats> {
+        return new Promise((resolve, reject) => {
+            this.model.find({ uuid: uuid }, (error, stats) => {
+                if (error) {
+                    console.error(`Unable to load stats for ${uuid}!`, error);
+                    reject(error);
+                } else {
+                    resolve(stats.length == 0 ? {} : stats[0]);
+                }
+            });
+        });
+    }
+
+    saveStats(uuid: string, stats: Stats): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.model.update(
+                { uuid: uuid },
+                new this.model({ uuid: uuid, data: stats }),
+                { overwrite: true }
+            );
+        });
+    }
 
 }
