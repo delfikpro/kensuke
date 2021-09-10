fcd75e95d04c7aaf82944d6942672a3ed41b8cee
delfikpro <33707322+DelfikPro@users.noreply.github.com>
2021-09-10 03:21:48 +0300

    Removed usage of node-cache

--- src/data/data-cache.ts
+++ src/data/data-cache.ts
@@ -1,30 +1,60 @@
 
 import { sessionStorage } from '@/session/session-storage';
-import NodeCache from 'node-cache';
+
 import { Dao } from './dao';
 
-export const dataCache = new NodeCache({
-    stdTTL: 60,
-    checkperiod: 10,
-    useClones: false,
-    deleteOnExpire: false
-}).on('expired', (key: string, value: Dao) => {
-    let sessionsLeft = sessionStorage.getSessionsByDataId(value.id).length;
-    if (sessionsLeft == 0) {
-        dataCache.del(key);
+export type CacheEntry = {
+    lastTouch: number,
+    dao: Dao 
+}
+
+export var dataCache = new Map<string, CacheEntry>();
+
+// export const dataCache = new NodeCache({
+//     stdTTL: 60,
+//     checkperiod: 10,
+//     useClones: false,
+//     deleteOnExpire: false
+// }).on('expired', (key: string, value: Dao) => {
+//     let sessionsLeft = sessionStorage.getSessionsByDataId(value.id).length;
+//     if (sessionsLeft == 0) {
+//         dataCache.del(key);
+//     }
+// });
+
+export function updateCache() {
+    const time = Date.now();
+    if (time - lastCacheUpdate > 10000) {
+        let newMap = new Map<string, CacheEntry>();
+        dataCache.forEach((v, k) => {
+            if (v.lastTouch + 60000 >= time || sessionStorage.getSessionsByDataId(k).length > 0) {
+                newMap.set(k, v);
+            }
+        });
+        dataCache = newMap;
+        lastCacheUpdate = Date.now();
     }
-});
+}
+
+var lastCacheUpdate = Date.now();
 
 export function getDao(dataId: string): Dao {
     
     if (dataCache.has(dataId)) {
-        const existing: Dao = dataCache.get(dataId);
-        if (existing) return existing;
+        const entry = dataCache.get(dataId);
+        entry.lastTouch = Date.now();
+
+        updateCache();
+        return entry.dao;
     }
 
     const dao = new Dao(dataId);
-    dataCache.set(dataId, dao);
+    dataCache.set(dataId, {
+        lastTouch: Date.now(), 
+        dao
+    });
 
+    updateCache();
     return dao;
 }
 
